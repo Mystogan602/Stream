9a3c87b5ec75d189eeef9436107b381dce487cb8
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-03-15 11:31:16 +0700

    setup: config graphQL

--- stream-backend/src/core/core.module.ts
+++ stream-backend/src/core/core.module.ts
@@ -1,6 +1,24 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { getGraphQLConfig } from './config/graphql.config'
+import { PrismaModule } from './prisma/prisma.module'
+import { ApolloDriver } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { IS_DEV_ENV } from 'src/shared/utils/is-dev.util'
+
 @Module({
-  imports: [PrismaModule],
+	imports: [
+		PrismaModule,
+		ConfigModule.forRoot({
+			isGlobal: true,
+			ignoreEnvFile: !IS_DEV_ENV
+		}),
+		GraphQLModule.forRootAsync({
+			driver: ApolloDriver,
+			useFactory: getGraphQLConfig,
+			inject: [ConfigService],
+			imports: [ConfigModule]
+		})
+	]
 })
 export class CoreModule {}
