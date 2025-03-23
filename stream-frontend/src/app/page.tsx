f4bbede88cef58c84c0f90e11b6af8326b9594bb
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-03-24 02:05:43 +0700

    feat: Add basic Ui components

--- stream-frontend/src/app/page.tsx
+++ stream-frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-
+import { Button } from '@/components/ui/common/Button';
 export default function Home() {
   const t = useTranslations('home');
 
   return <div className='flex flex-col items-center justify-center h-screen'>
     <h1 className='text-4xl font-bold'>{t('title')}</h1>
+    <Button>Click me</Button>
+    <Button variant='outline'>Click me</Button>
+    <Button variant='secondary'>Click me</Button>
+    <Button variant='ghost'>Click me</Button>
   </div>;
 }
