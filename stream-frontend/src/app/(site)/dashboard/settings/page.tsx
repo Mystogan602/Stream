8123bf6a1c21805926d56907f01ef92f878f8c3b
Phạm Minh Tiến <phminhtien136@gmail.com>
2025-03-27 15:35:20 +0700

    feat(setting): setting tabs ui

--- stream-frontend/src/app/(site)/dashboard/settings/page.tsx
+++ stream-frontend/src/app/(site)/dashboard/settings/page.tsx
@@ -1,9 +1,20 @@
 import type { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 
-export const metadata: Metadata = {
-	title: 'Dashboard Settings',
-};
+import { UserSettings } from '@/components/features/user/UserSetting';
+
+export async function generateMetadata(): Promise<Metadata> {
+	const t = await getTranslations('dashboard.settings.header');
+	return {
+		title: t('heading'),
+		description: t('description'),
+		robots: {
+			index: false,
+			follow: false
+		}
+	};
+}
 
 export default function SettingsPage() {
-	return <div>Dashboard Settings</div>;
+	return <UserSettings />;
 }
